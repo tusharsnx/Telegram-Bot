4ff89b42d0c3dbc5fd19447f85ae4951a747d766
Tushar Singh <tusharvickey1999@gmail.com>
2021-08-24 20:23:47 +0530

    added debug lines

--- responses/generate_responses.py
+++ responses/generate_responses.py
@@ -26,8 +26,10 @@ active_users_state = dict()
 
 async def generate_response(text, username):
     print(active_users_state)
+    print(id(active_users_state))
     # user is active
     if username in active_users:
+        
         print("user found active")
         state_id = active_users_state[username]
         next_state_id, resp = await states[state_id](text, username)
